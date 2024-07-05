88c550511bc9bec1cd843f8ec713d9e7805fd0bc
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-05 10:57:21 +0300

    navigation-activity | Add List Activity

---
+++ app/src/main/java/com/example/interntasks_2a/Case1/ListActivity.kt
@@ -0,0 +1,15 @@
+package com.example.interntasks_2a.Case1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.interntasks_2a.R
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
