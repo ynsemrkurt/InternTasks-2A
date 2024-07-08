8b61c679c3d489e2a3e46c42d0a5ea7126583133
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-08 16:44:01 +0300

    navigation-fragment | Details Fragment completed

--- app/src/main/java/com/example/interntasks_2a/case2/DetailsFragment.kt
+++ app/src/main/java/com/example/interntasks_2a/case2/DetailsFragment.kt
@@ -1,22 +1,49 @@
 package com.example.interntasks_2a.case2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.interntasks_2a.R
+import androidx.fragment.app.Fragment
+import com.example.interntasks_2a.Constants.CITY_KEY
+import com.example.interntasks_2a.Constants.DEGREE_KEY
+import com.example.interntasks_2a.Constants.STATUS_KEY
+import com.example.interntasks_2a.WeatherUtils
+import com.example.interntasks_2a.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    ): View {
+        binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            val degree = it.getInt(DEGREE_KEY, 0)
+            val city = it.getString(CITY_KEY)
+            val status = it.getString(STATUS_KEY)
+
+            WeatherUtils.bindWeatherData(
+                binding.textViewDegree,
+                binding.textViewCity,
+                binding.textViewStatus,
+                binding.imageViewStatus,
+                degree,
+                city.toString(),
+                status,
+                requireContext()
+            )
+        }.run {
+            Log.e("Null Error", "arguments is null")
+        }
     }
 }
