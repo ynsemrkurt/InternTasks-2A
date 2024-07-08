544b56e6a6d571a18d8445f4245088f8c863c218
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-08 14:51:07 +0300

    navigation-fragment | Create Weather Activity

---
+++ app/src/main/java/com/example/interntasks_2a/case2/WeatherActivity.kt
@@ -0,0 +1,15 @@
+package com.example.interntasks_2a.case2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.interntasks_2a.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
