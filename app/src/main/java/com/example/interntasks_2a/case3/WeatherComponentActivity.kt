fd5849dd825ee5570d16f629da2b8b2d62826533
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-10 19:51:46 +0300

    navigation-component | Add goActivity fun

--- app/src/main/java/com/example/interntasks_2a/case3/WeatherComponentActivity.kt
+++ app/src/main/java/com/example/interntasks_2a/case3/WeatherComponentActivity.kt
@@ -1,10 +1,7 @@
 package com.example.interntasks_2a.case3
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.interntasks_2a.R
 
 class WeatherComponentActivity : AppCompatActivity() {
