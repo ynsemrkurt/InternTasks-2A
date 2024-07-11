bab6e76e3eed39f5295d77176d215f495e0759c6
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-11 11:08:11 +0300

    Add id column data class and list

--- app/src/main/java/com/example/interntasks_2a/Weather.kt
+++ app/src/main/java/com/example/interntasks_2a/Weather.kt
@@ -1,6 +1,7 @@
 package com.example.interntasks_2a
 
 data class Weather(
+    val id: Int,
     val degree: Int,
     val degreeMin: Int,
     val degreeMax: Int,
@@ -9,12 +10,12 @@ data class Weather(
 )
 
 val weatherList = listOf(
-    Weather(10, 5, 15, "İstanbul", "Güneşli"),
-    Weather(15, 10, 20, "Ankara", "Bulutlu"),
-    Weather(20, 15, 25, "İzmir", "Yağmurlu"),
-    Weather(25, 20, 30, "Antalya", "Karlı"),
-    Weather(30, 25, 35, "Bursa", "Güneşli"),
-    Weather(35, 30, 40, "Konya", "Bulutlu"),
-    Weather(40, 35, 45, "Diyarbakır", "Yağmurlu"),
-    Weather(45, 40, 50, "Edirne", "Karlı")
+    Weather(1,10, 5, 15, "İstanbul", "Güneşli"),
+    Weather(2,15, 10, 20, "Ankara", "Bulutlu"),
+    Weather(3,20, 15, 25, "İzmir", "Yağmurlu"),
+    Weather(4,25, 20, 30, "Antalya", "Karlı"),
+    Weather(5,30, 25, 35, "Bursa", "Güneşli"),
+    Weather(6,35, 30, 40, "Konya", "Bulutlu"),
+    Weather(7,40, 35, 45, "Diyarbakır", "Yağmurlu"),
+    Weather(8,45, 40, 50, "Edirne", "Karlı")
 )
